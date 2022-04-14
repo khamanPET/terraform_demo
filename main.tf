d98a552fc41a4cf0fc763b18b9136b7936014cef
khamanPET <102649308+khamanPET@users.noreply.github.com>
2022-04-14 10:58:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "random_pet" "petname" {
+resource "random_pet" "petname3" {
   length    = 3
+  separator = "-"
+}
+resource "random_pet" "petname2" {
+  length    = 2
   separator = "-"
 }
